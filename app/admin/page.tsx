677236dd8d272553c91d4fe5629314214a7938e0
Huskymillion <pfohl.pascal@web.de>
2026-05-04 18:24:15 +0200

    Bugfix

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,10 +4,12 @@ import { useEffect } from "react";
 
 export default function AdminPage() {
   useEffect(() => {
-    import("decap-cms-app").then((CMS) => {
-      CMS.default.init();
-    });
+    window.location.replace("/admin/index.html");
   }, []);
 
-  return <div id="nc-root" />;
+  return (
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", fontFamily: "monospace", fontSize: "0.8rem", opacity: 0.4 }}>
+      loading cms…
+    </div>
+  );
 }
