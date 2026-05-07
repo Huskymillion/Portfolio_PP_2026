d6b3bdc6d56bb88fe885280c3123298d60303e24
Huskymillion <pfohl.pascal@web.de>
2026-05-07 10:51:36 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import { SmoothScrollProvider } from "@/components/SmoothScrollProvider";
 import { PillNav } from "@/components/PillNav";
 import "./globals.css";
@@ -44,6 +45,12 @@ export default function RootLayout({
           {children}
           <PillNav />
         </SmoothScrollProvider>
+        <Script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "dc4e4fd3c646463fb2d6603bd91bc876"}'
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
